835efd51805e9fcc2694d50ce00982e2156c1e44
donpachi <aaron.th.kim@gmail.com>
2016-11-24 15:25:26 -0700

    Skeleton activity

---
+++ app/src/main/java/com/obdii/seng521/obdiireader/MainScreenActivity.java
@@ -0,0 +1,13 @@
+package com.obdii.seng521.obdiireader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
